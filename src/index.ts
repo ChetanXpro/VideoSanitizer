d603e62a6942f39dee1eff5a24f9ac67f82be740
Chetan baliyan <chetanbaliyan10@gmail.com>
2024-01-29 23:50:38 +0530

    feat: add functions for timestamp and llm call

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,26 @@
 import dotenv from 'dotenv'
+import { findCurseWordsTimestamp } from './services/ai.service'
+import { createTranscription } from './services/transcription.service'
 dotenv.config()
 
+interface IArgs {
+	absolutePathToVideo: string
+}
+
+const run = async ({ absolutePathToVideo }: IArgs) => {
+	// TODO: Create .srt for given video
+	const createTranscriptionResult = await createTranscription(absolutePathToVideo)
+	console.log(createTranscriptionResult)
+
+	const srt_content = findCurseWordsTimestamp({
+		srt_content: 'Yoooo',
+	})
+
+	// TODO: Check in .srt if there are any bad words , and if exist then return them
+	// TODO: Create a new .srt file with bad words replaced with beep sound
+	// TODO: Add new .srt to video
+}
+
+run({
+	absolutePathToVideo: '/Users/chetan/Developer/code/video-scanner/sample.mp4',
+})
